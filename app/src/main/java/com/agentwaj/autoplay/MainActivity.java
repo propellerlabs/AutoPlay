a89ae81aa6cc585bd69032d78c49d2eb2b756baf
Waj Siddiqui <agentwaj@gmail.com>
2017-06-17 15:36:09 +0100

    Add dummy list

--- app/src/main/java/com/agentwaj/autoplay/MainActivity.java
+++ app/src/main/java/com/agentwaj/autoplay/MainActivity.java
@@ -1,7 +1,11 @@
 package com.agentwaj.autoplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((ListView) findViewById(R.id.list)).setAdapter(new MyAdapter(getItems()));
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            items.add("Item " + i);
+        }
+
+        return items;
     }
 }
---
+++ app/src/main/java/com/agentwaj/autoplay/MyAdapter.java
@@ -0,0 +1,44 @@
+package com.agentwaj.autoplay;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+class MyAdapter extends BaseAdapter {
+
+    private List<String> items;
+
+    MyAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        ((TextView) convertView.findViewById(android.R.id.text1)).setText(items.get(position));
+
+        return convertView;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+}
