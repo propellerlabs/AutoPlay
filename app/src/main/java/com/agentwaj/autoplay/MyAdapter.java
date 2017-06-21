1d90b51984d440b72351a62ee587654d728ca5ee
Waj Siddiqui <agentwaj@gmail.com>
2017-06-21 01:45:51 +0100

    Replace VideoView with TextureView for UI sync buffers

--- app/src/main/java/com/agentwaj/autoplay/MyAdapter.java
+++ app/src/main/java/com/agentwaj/autoplay/MyAdapter.java
@@ -1,12 +1,11 @@
 package com.agentwaj.autoplay;
 
-import android.media.MediaPlayer;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.danikula.videocache.HttpProxyCacheServer;
 
@@ -18,14 +17,12 @@ class MyAdapter extends BaseAdapter {
     private static final int VIEW_TYPE_ARTICLE = 1;
     private static final int VIEW_TYPE_COUNT = 2;
 
-    private HttpProxyCacheServer proxy;
     private List<String> items;
     private VideoTracker tracker;
 
     MyAdapter(ViewGroup container, HttpProxyCacheServer proxy, List<String> items) {
-        this.proxy = proxy;
         this.items = items;
-        tracker = new VideoTracker(container);
+        tracker = new VideoTracker(container, proxy);
     }
 
     @Override
@@ -42,21 +39,9 @@ class MyAdapter extends BaseAdapter {
                 final String description = "Item " + position;
                 ((TextView) convertView.findViewById(R.id.description)).setText(description);
 
-                final VideoView videoView = (VideoView) convertView.findViewById(R.id.video);
-                String source = "http://mirrors.standaloneinstaller.com/video-sample/lion-sample.3gp";
-
-                // https://stackoverflow.com/a/28485640
-                videoView.setVisibility(View.GONE);
-                videoView.setVisibility(View.VISIBLE);
-
-                videoView.setVideoPath(proxy.getProxyUrl(source));
-                videoView.start();
-                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        tracker.startTracking(description, videoView);
-                    }
-                });
+                TextureView textureView = (TextureView) convertView.findViewById(R.id.video);
+                textureView.setSurfaceTextureListener(tracker);
+                tracker.startTracking(description, textureView);
                 break;
             default:
                 ((TextView) convertView.findViewById(android.R.id.text1)).setText(items.get(position));
--- app/src/main/java/com/agentwaj/autoplay/VideoTracker.java
+++ app/src/main/java/com/agentwaj/autoplay/VideoTracker.java
@@ -1,32 +1,42 @@
 package com.agentwaj.autoplay;
 
 import android.graphics.Rect;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
 import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.VideoView;
+
+import com.danikula.videocache.HttpProxyCacheServer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-class VideoTracker {
+class VideoTracker implements TextureView.SurfaceTextureListener {
 
     private static final String TAG = VideoTracker.class.getName();
 
-    private Map<String, VideoView> viewsBeingTracked;
+    private Map<String, TextureView> viewsToBeTracked;
+    private Map<String, TextureView> viewsBeingTracked;
+    private HttpProxyCacheServer proxy;
+    private MediaPlayer mediaPlayer;
 
-    VideoTracker(ViewGroup container) {
+    VideoTracker(ViewGroup container, HttpProxyCacheServer proxy) {
+        this.proxy = proxy;
 
+        viewsToBeTracked = new HashMap<>();
         viewsBeingTracked = new HashMap<>();
 
         container.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
                 for (String key : viewsBeingTracked.keySet()) {
-                    VideoView videoView = viewsBeingTracked.get(key);
+                    TextureView textureView = viewsBeingTracked.get(key);
 
                     Rect viewRect = new Rect();
-                    if (!videoView.getLocalVisibleRect(viewRect) || !videoView.isShown()) {
+                    if (!textureView.getLocalVisibleRect(viewRect) || !textureView.isShown()) {
                         log(key + " is not visible");
                         continue;
                     }
@@ -34,7 +44,7 @@ class VideoTracker {
                     int visibleWidth = viewRect.right - viewRect.left;
                     int visibleHeight = viewRect.bottom - viewRect.top;
                     int visibleArea = visibleWidth * visibleHeight;
-                    int viewArea = videoView.getWidth() * videoView.getHeight();
+                    int viewArea = textureView.getWidth() * textureView.getHeight();
                     int visibleAreaPercent = viewArea == 0 ? 0 :
                             (int) (((float) visibleArea) / viewArea * 100f);
                     log(key + " is " + visibleAreaPercent + "% visible");
@@ -43,12 +53,76 @@ class VideoTracker {
         });
     }
 
-    void startTracking(String key, VideoView videoView) {
-        viewsBeingTracked.put(key, videoView);
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+        String source = "http://mirrors.standaloneinstaller.com/video-sample/lion-sample.3gp";
+        Surface surface = new Surface(surfaceTexture);
+        try {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(proxy.getProxyUrl(source));
+            mediaPlayer.setSurface(surface);
+            mediaPlayer.prepareAsync();
+//            mediaPlayer.setOnBufferingUpdateListener(this);
+//            mediaPlayer.setOnCompletionListener(this);
+//            mediaPlayer.setOnVideoSizeChangedListener(this);
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mediaPlayer.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+            updateTrackedViews();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    void stopTracking(String key) {
-        viewsBeingTracked.remove(key);
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    private void updateTrackedViews() {
+        for (String key : viewsToBeTracked.keySet()) {
+            viewsBeingTracked.put(key, viewsToBeTracked.remove(key));
+        }
+    }
+
+    void startTracking(final String key, final TextureView textureView) {
+        viewsToBeTracked.put(key, textureView);
+    }
+
+    void stopTracking(final TextureView textureView) {
+        if (textureView == null) {
+            return;
+        }
+        for (String key : viewsBeingTracked.keySet()) {
+            if (viewsBeingTracked.get(key).equals(textureView)) {
+                viewsBeingTracked.remove(key);
+            }
+        }
+
+        for (String key : viewsToBeTracked.keySet()) {
+            if (viewsToBeTracked.get(key).equals(textureView)) {
+                viewsToBeTracked.remove(key);
+            }
+        }
     }
 
     private static void log(String message) {
